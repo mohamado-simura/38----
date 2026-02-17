e0184a4002b6f861f7e255901ebcb53280862a6d
mohamado-simura <mohamado@simura.com>
2026-02-17 11:31:56 +0900

    python first month

---
+++ Python/test.py
@@ -0,0 +1,34 @@
+x=13
+y=29
+c=(y-x)
+print(c)
+# if from
+if 5>2:
+    print("five is greater than two")
+# step into a little bit complex
+print("Hello,World")
+x="I'm Arnob . From Bangladesh"
+print(x)
+# time to step up
+print("Hi,i'm Arnob",end="I'm From Bangladesh")
+#now mix letter with variable and numbers
+print("Hello.I'm Arnob",32,end="years old")
+#today we learn numbers
+x_new,y,z = "Assaduzzaman","Md","Arnob"
+print(x_new)
+print(y)
+print(z)
+#now learn about collectionof items
+fruits=["apple","orange","grape"]
+print(fruits)
+#now we learn about character postition
+a ="Hello,World"
+print(a[6])
+# learn about len 
+a ="hello,Assaduzzaman"
+print(len(a))
+# learn in 
+txt="assaduzzaman is not a intilligent boy "
+print("intilligent" in txt)
+if"brilliant"not in txt:
+    print("sorry to say he is dumb")
--- シムシムジャパン/プログラミング/javaScriptstest.js
+++ シムシムジャパン/プログラミング/javaScriptstest.js
@@ -1,4 +1,24 @@
-// its a simple starting code 
+//  its a simple starting code
 console.log("John Doe ");
 console.log("19");
 console.log("Student");
+// now make it a bit complex
+
+let message =" hello , my name is john doe .I am 19 years old and i am a student.";
+console.log(message);
+
+let message_1 =" Hello, Dave";
+console.log(message_1);
+
+// now use some numbers on this 
+
+let name ="Assaduzzaman Arnob";
+let age ="19";
+console.log(name + "and my age is" + age);
+
+let score ="10" + "30";
+console.log(score);
+
+let Score_1 = 15;
+let score_2 = 23;
+console.log(Score_1 + score_2);
